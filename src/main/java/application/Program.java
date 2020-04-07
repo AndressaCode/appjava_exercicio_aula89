3616fe53aa8c0f44813235817690e25b878e8632
Andressa <andressaszx@gmail.com>
2020-04-07 20:15:13 -0300

    Minha versão final

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -31,10 +31,50 @@
  */
 package application;
 
+import entities.Rent;
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author Andressa Silva
  */
 public class Program {
+    
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        
+        Rent[] vect = new Rent[10];
+        
+        System.out.print("How many rooms will be rented? ");
+        int r = sc.nextInt();
+        
+        
+        
+        for(int i=0; i<r; i++){
+            sc.nextLine(); // para consumir o enter acima
+            System.out.printf("Rent #%d %n", i+1);
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+            System.out.print("Room: ");
+            int room = sc.nextInt();
+            System.out.println();
+            
+            vect[i] = new Rent(name, email, room);
+        }
+        
+        System.out.println("Busy rooms: ");
+        System.out.println();
+        for(int i=0; i<vect.length; i++){
+            System.out.print(vect[i].getRoom() + ": " +
+                             vect[i].getName() + ", " +  
+                             vect[i].getEmail());
+            System.out.println();
+        }
+        sc.close();
+    }
     
 }
